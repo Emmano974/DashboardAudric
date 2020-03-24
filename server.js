00f57f879f5445399ae6c699cd77564a5699bb06
AudricL <audriclamy@hotmail.fr>
2020-03-24 20:29:07 +0400

    Init commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/api', function (req, res) {
+    res.send("api")
+  });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8000)
+
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const path = require('path');
+// const express = require('express')
+// const app = express()
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/',  (req, res)=> {
+//     sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(process.env.PORT || 8000);
+
+
